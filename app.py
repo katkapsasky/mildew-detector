334bb2c3029365ca1cfc1a27f4be336002e08621
katkapsasky <>
2023-06-08 08:07:31 +0000

    Create instance and add app pages

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ from app_pages.page_cells_visualizer import page_leaf_visualizer_body
 from app_pages.page_malaria_detector import page_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
+
+# Create an instance of the app
+app = MultiPage(app_name= "Malaria Detector")  
+
+# Add app pages using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Cells Visualizer", page_cells_visualizer_body)
+app.add_page("Malaria Detection", page_malaria_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+# Run the app
+app.run() 
