5299c885ddc7c15fb0868e83c026e5c9234441e5
katkapsasky <>
2023-06-08 09:20:21 +0000

    Add basic info for hypothesis page

---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,17 @@
+# Import packages
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"* We suspect cherry leaves infected with powdery mildew "
+        f"have clear marks/signs, that can differentiate, from a healthy leaf. \n\n"
+        f"* An Image Montage, shows that typically an infected leaf has.. "
+        f"Average Image, Variability Image and Difference between Averages "
+        f"studies didn't reveal "
+        f"any clear pattern to differentiate one to another."
+
+    )
