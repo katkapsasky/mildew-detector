2637f85efb34e4f273cbfb8484303aecadeca0c2
katkapsasky <>
2023-06-08 09:28:47 +0000

    Fix EOL syntax error

--- src/data_management.py
+++ src/data_management.py
@@ -6,22 +6,20 @@ import base64
 from datetime import datetime
 import joblib
 
-
 def download_dataframe_as_csv(df):
-    """
-    Function to download dataframe as csv file
-    """
+
     datetime_now = datetime.now().strftime("%d%b%Y_%Hh%Mmin%Ss")
     csv = df.to_csv().encode()
     b64 = base64.b64encode(csv).decode()
     href = (
-        f'
-        <a href="data:file/csv;base64,{b64}" 
-        download="Report {datetime_now}.csv"
-        '
+        f'<a href="data:file/csv;base64,{b64}" download="Report {datetime_now}.csv" '
         f'target="_blank">Download Report</a>'
     )
     return href
+
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
 
 
 def load_pkl_file(file_path):
