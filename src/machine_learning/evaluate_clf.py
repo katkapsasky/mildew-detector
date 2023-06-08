cfafd1f4e728b0571e5cdc8ae2767849530dce42
katkapsasky <>
2023-06-08 09:35:15 +0000

    Fix indentation error on return statement

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -7,4 +7,4 @@ def load_test_evaluation(version):
     """
     Function to load test evaluation pickle file
     """
-  return load_pkl_file(f'outputs/{version}/evaluation.pkl')
+    return load_pkl_file(f'outputs/{version}/evaluation.pkl')
