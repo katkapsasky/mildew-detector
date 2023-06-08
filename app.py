a1b4048d320baa51ef4c03ece53e915d85120aa9
katkapsasky <>
2023-06-08 08:06:48 +0000

    Load page scripts

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 # Import streamlit
 import streamlit as st
 from app_pages.multipage import MultiPage
+
+# Load page scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_cells_visualizer import page_leaf_visualizer_body
+from app_pages.page_malaria_detector import page_mildew_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
