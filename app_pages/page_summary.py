cd78ade1523e985489a0f108fc1b130b9e58bf99
katkapsasky <>
2023-06-10 06:57:52 +0000

    Add general project info

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -7,7 +7,21 @@ def page_summary_body():
     st.write("### Quick Project Summary")
 
     st.info(
-        f"**General Information**")
+        f"**General Information**"
+        f"Powdery mildew is a fungal disease of the foliage and "
+        f"stems of a plant, where a superficial fungal growth covers the "
+        f"surface of the plant.\n"
+        f"The disease can be visually identified by white, powdery "
+        f"spreading patches of fungus on a leaf's surface.\n"
+        f"Leaves infected with powdery mildew may gradually turn completely "
+        "yellow, die, and fall off, which can expose fruit to sunburn. "
+        f"Severely infected plants can result in reduced yields, shortened "
+        f"production times, and fruit that has little flavor.")
+
+    st.info(
+        f"**Dataset Content**"
+        f"The dataset, provided by the client, contains 4208 images of cherry "
+        f"leaves; 2104 healthy and 2104 powdery mildew infected leaves. ")
 
     st.write(
         f"* For additional information, please visit and **read** the "
@@ -17,7 +31,7 @@ def page_summary_body():
         f"The project has 2 business requirements:\n"
         f"* 1 - The client is interested in conducting a study to visually "
         f"differentiate a cherry leaf that is healthy from one that "
-        f"contains powdery mildew..\n"
+        f"contains powdery mildew.\n"
         f"* 2 - The client is interested in predicting if a cherry leaf is "
         f"healthy or contains powdery mildew. "
     )
