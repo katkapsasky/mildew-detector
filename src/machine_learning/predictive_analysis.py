1116e7cbdd04ec0d2a57e797940fe93bcef2faa4
katkapsasky <>
2023-06-11 16:44:52 +0000

    Remove trailing whitespaces

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -13,27 +13,28 @@ def plot_predictions_probabilities(pred_proba, pred_class):
     Function to plot prediction probability results
     """
 
-    prob_per_class= pd.DataFrame(
-            data=[0,0],
-            index={'Healthy': 0, 'Infected': 1}.keys(),
-            columns=['Probability']
-        )
+    prob_per_class = pd.DataFrame(
+        data=[0, 0],
+        index={'Healthy': 0, 'Infected': 1}.keys(),
+        columns=['Probability']
+    )
     prob_per_class.loc[pred_class] = pred_proba
     for x in prob_per_class.index.to_list():
-        if x not in pred_class: prob_per_class.loc[x] = 1 - pred_proba
+        if x not in pred_class:
+            prob_per_class.loc[x] = 1 - pred_proba
     prob_per_class = prob_per_class.round(3)
     prob_per_class['Diagnostic'] = prob_per_class.index
-    
+
     fig = px.bar(
-            prob_per_class,
-            x = 'Diagnostic',
-            y = prob_per_class['Probability'],
-            range_y=[0,1],
-            width=600, height=300,template='seaborn')
+        prob_per_class,
+        x='Diagnostic',
+        y=prob_per_class['Probability'],
+        range_y=[0, 1],
+        width=600, height=300, template='seaborn')
     st.plotly_chart(fig)
 
 
-def resize_input_image(img, version):  
+def resize_input_image(img, version):
     """
     Function to reshape image to average image size
     """
@@ -51,15 +52,15 @@ def load_model_and_predict(my_image, version):
 
     model = load_model(f"outputs/{version}/mildew_detector_model.h5")
 
-    pred_proba = model.predict(my_image)[0,0]
+    pred_proba = model.predict(my_image)[0, 0]
 
     target_map = {v: k for k, v in {'Healthy': 0, 'Infected': 1}.items()}
-    pred_class =  target_map[pred_proba < 0.5]  
-    if pred_class == target_map[1]: pred_proba = 1 - pred_proba
-
+    pred_class = target_map[pred_proba < 0.5]
+    if pred_class == target_map[1]:
+        pred_proba = 1 - pred_proba
 
     st.write(
         f"The predictive analysis indicates the sample leaf is "
         f"**{pred_class.lower()}**")
-    
+
     return pred_proba, pred_class
